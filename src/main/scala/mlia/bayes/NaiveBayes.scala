ea5d9641d629c387a2a98cf6a22cab4922270338
noboru kudoh <noboru.kudoh@gmail.com>
2013-10-26 09:39:55 +0900

    a bit refactor.

--- src/main/scala/mlia/bayes/NaiveBayes.scala
+++ src/main/scala/mlia/bayes/NaiveBayes.scala
@@ -7,13 +7,13 @@ object NaiveBayes {
 
   case class Prob(num: Vector[Int], denom: Double) {
 
-    def probability: Vector[Double] = num.mapActiveValues(_.toDouble) :/ denom
+    def probability: Vector[Double] = num.mapValues(_.toDouble) :/ denom
 
-    def logProbability: Vector[Double] = log(num.mapActiveValues(_.toDouble) :/ denom)
+    def logProbability: Vector[Double] = log(num.mapValues(_.toDouble) :/ denom)
   }
 
   object Prob {
-    def apply(size: Int): Prob = Prob(DenseVector.ones(size), 2.0d)
+    def apply(size: Int): Prob = Prob(DenseVector.ones(size), 2.0d) // avoid 0
   }
 
   def trainNB0(trainMatrix: DenseMatrix[Int], trainCategory: Vector[Int]): (Prob, Prob, Double) = {
@@ -21,16 +21,13 @@ object NaiveBayes {
     val numTrainDocs = trainMatrix.rows
     val numWords = trainMatrix.cols
 
-    // initialize probabilistic, calculate class=1 probability
-    val pAbusive = trainCategory.sum / numTrainDocs.toDouble
-
     val probs = Range(0, numTrainDocs).foldLeft((Prob(numWords), Prob(numWords))) { (state, i) =>
       val v: Vector[Int] = trainMatrix(i, ::).toDenseVector // [0, 1, 0, 0, 1, 0...]
       // vector addition
       if (trainCategory(i) == 1) (Prob(state._1.num + v, state._1.denom + v.sum), state._2) // add up class=1
       else (state._1, Prob(state._2.num + v, state._2.denom + v.sum))
     }
-    (probs._2, probs._1, pAbusive) // probability, class=0, class=1, abusive
+    (probs._2, probs._1, trainCategory.sum / numTrainDocs.toDouble) // probability, class=0, class=1, abusive
   }
 
   def classifyNB(vec2Classify: Vector[Int], p0Vec: Vector[Double], p1Vec: Vector[Double], pClass1: Double) = {
---
+++ src/main/scala/mlia/lr/LogisticRegression.scala
@@ -0,0 +1,5 @@
+package mlia.lr
+
+object LogisticRegression {
+
+}
