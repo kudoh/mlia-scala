78318434a2f5c1602a8ccd876c800e88e75fa7f3
noboru kudoh <noboru.kudoh@gmail.com>
2013-10-26 16:56:16 +0900

    add stochastic gradient ascent version 0

--- src/main/scala/mlia/lr/LogisticRegression.scala
+++ src/main/scala/mlia/lr/LogisticRegression.scala
@@ -5,26 +5,35 @@ import breeze.numerics._
 
 object LogisticRegression {
 
-  val alpha = 0.001
-  val maxCycle = 500
   val sigmoid = breeze.generic.UFunc((x: Double) => 1.0 / (1 + exp(-1 * x)))
 
-  def gradAscent(dataMatIn: List[Array[Double]], classLabels: Array[Int]) = {
+  def gradAscent(dataMatIn: List[Array[Double]], classLabels: Array[Int]): DenseVector[Double] = {
+
+    val alpha = 0.001
+    val maxCycle = 500
     val dataMatrix = DenseMatrix(dataMatIn: _*)
     val labelMat = DenseVector(classLabels.map(_.toDouble))
-    Range(1, maxCycle).foldLeft(DenseMatrix.ones[Double](dataMatrix.cols, 1)) { (curWeight, cycle) =>
-      val dot1: DenseVector[Double] = dot(dataMatrix, curWeight)
-      val h: DenseVector[Double] = sigmoid(dot1)
-      if (cycle == 100) println(dot1)
+
+    Range(0, maxCycle).foldLeft(DenseMatrix.ones[Double](dataMatrix.cols, 1)) { (curWeight, cycle) =>
+      val h: DenseVector[Double] = sigmoid(dot(dataMatrix, curWeight))
       val error: DenseVector[Double] = labelMat :- h
-      val addition: DenseMatrix[Double] = dataMatrix.t :* alpha
-      val dotError: DenseVector[Double] = dot(addition, error.toDenseMatrix)
-      val weights = curWeight :+ dotError.toDenseMatrix
+      val weights = curWeight :+ dot(dataMatrix.t :* alpha, error.toDenseMatrix).toDenseMatrix
       if (cycle % 100 == 0) println(s"$cycle : ${weights.toDenseVector.toString()}")
       weights
+    }.toDenseVector
+  }
+
+  def stocGradAscent0(dataMatIn: List[Array[Double]], classLabels: Array[Int]): DenseVector[Double] = {
+    val alpha = 0.01
+    dataMatIn.zipWithIndex.foldLeft(DenseVector.ones[Double](dataMatIn.head.size)) { (curWeight, row) =>
+      val (value, i) = row
+      val vec = DenseVector(value)
+      val h = sigmoid(dot(vec.toDenseMatrix, curWeight.toDenseMatrix).sum)
+      val error = classLabels(i) - h
+      curWeight :+ (vec :* (alpha * error): DenseVector[Double])
     }
   }
 
-  def dot(x: DenseMatrix[Double], y: DenseMatrix[Double]): DenseVector[Double] =
+  private def dot(x: DenseMatrix[Double], y: DenseMatrix[Double]): DenseVector[Double] =
     DenseVector(Range(0, x.rows).map(i => (x(i, ::) :* y.t).sum).toArray)
 }
