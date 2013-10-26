499007aad209df9086f8c77b94ba3e175641cb44
noboru kudoh <noboru.kudoh@gmail.com>
2013-10-26 16:08:53 +0900

    add logistic regression trainer

--- src/main/scala/mlia/lr/LogisticRegression.scala
+++ src/main/scala/mlia/lr/LogisticRegression.scala
@@ -1,5 +1,30 @@
 package mlia.lr
 
+import breeze.linalg._
+import breeze.numerics._
+
 object LogisticRegression {
 
+  val alpha = 0.001
+  val maxCycle = 500
+  val sigmoid = breeze.generic.UFunc((x: Double) => 1.0 / (1 + exp(-1 * x)))
+
+  def gradAscent(dataMatIn: List[Array[Double]], classLabels: Array[Int]) = {
+    val dataMatrix = DenseMatrix(dataMatIn: _*)
+    val labelMat = DenseVector(classLabels.map(_.toDouble))
+    Range(1, maxCycle).foldLeft(DenseMatrix.ones[Double](dataMatrix.cols, 1)) { (curWeight, cycle) =>
+      val dot1: DenseVector[Double] = dot(dataMatrix, curWeight)
+      val h: DenseVector[Double] = sigmoid(dot1)
+      if (cycle == 100) println(dot1)
+      val error: DenseVector[Double] = labelMat :- h
+      val addition: DenseMatrix[Double] = dataMatrix.t :* alpha
+      val dotError: DenseVector[Double] = dot(addition, error.toDenseMatrix)
+      val weights = curWeight :+ dotError.toDenseMatrix
+      if (cycle % 100 == 0) println(s"$cycle : ${weights.toDenseVector.toString()}")
+      weights
+    }
+  }
+
+  def dot(x: DenseMatrix[Double], y: DenseMatrix[Double]): DenseVector[Double] =
+    DenseVector(Range(0, x.rows).map(i => (x(i, ::) :* y.t).sum).toArray)
 }
