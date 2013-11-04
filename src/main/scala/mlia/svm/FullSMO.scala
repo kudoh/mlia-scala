5a18442770c20955bafaadeb9c50377060c907eb
noboru.kudoh <noboru.kudoh@gmail.com>
2013-11-04 23:10:33 +0900

    add Full Platt SMO algorithm

--- src/main/scala/mlia/svm/FullSMO.scala
+++ src/main/scala/mlia/svm/FullSMO.scala
@@ -1,55 +1,186 @@
 package mlia.svm
 
+import scala.annotation.tailrec
 import breeze.linalg._
 import breeze.numerics._
+import breeze.stats.distributions.Uniform
 
 object FullSMO {
 
-  case class OptStruct(dataMat: DenseMatrix[Double],
-                       labelMat: DenseMatrix[Double],
-                       alphas: DenseMatrix[Double],
+  type Mat = DenseMatrix[Double]
+
+  case class OptStruct(dataMat: Mat,
+                       labelMat: Mat,
+                       alphas: Mat,
                        b: Double = 0.0,
                        constant: Double,
-                       tolerance: Double,
-                       eCache: DenseMatrix[Double]) {
+                       tolerance: Double) {
+
+    val eCache: Mat = DenseMatrix.zeros[Double](dataMat.rows, 2)
+
+    def label(i: Int): Double = labelMat(i, 0)
 
     val rows = dataMat.rows
 
-    def add(i: Int, ei: Double): OptStruct = {
+    def alpha(idx: Int) = alphas(idx, 0)
+
+    def validEcacheArr: Array[Int] = eCache(::, 0).findAll(_ != 0).toArray
+
+    def calcETA(i: Int, j: Int): Double = {
+      ((((dataMat(i, ::) * dataMat(j, ::).t: Mat) :* 2.0: Mat) :-
+        dataMat(i, ::) * dataMat(i, ::).t: Mat) :- dataMat(j, ::) *
+        dataMat(j, ::).t: Mat)(0, 0)
+    }
+
+    def calcEk(k: Int): Double = {
+      val fXk: Mat = ((alphas :* labelMat: Mat).t * (dataMat * dataMat(k, ::).t: Mat): Mat) :+ b
+      fXk(0, 0) - label(k)
+    }
+
+    def nonBoundIndices: Seq[Int] = alphas.findAll(x => x > 0 && x < constant).map(_._1)
+
+    def cache(i: Int, ei: Double) {
       eCache(i, ::) := DenseVector(1, ei)
-      copy(eCache = eCache)
     }
 
-    def updateEk(k: Int): OptStruct = {
-       val ek = calcEk(this, k)
-       add(k, ek)
+    def updateEk(k: Int) {
+      cache(k, calcEk(k))
     }
-  }
 
-  def calcEk(oS: OptStruct, k: Int) = {
-    val fXk: DenseMatrix[Double] = ((oS.alphas :* oS.labelMat: DenseMatrix[Double]).t * (oS.dataMat * oS.dataMat(k, ::).t: DenseMatrix[Double]): DenseMatrix[Double]) :+ oS.b
-    fXk(0, 0) - oS.labelMat(k, 0)
+    def newB(b: Double) = {
+      copy(b = b)
+    }
   }
 
   case class JOpt(maxK: Int = -1, maxDeltaE: Double = 0.0, ej: Double = 0.0)
 
-  def selectJ(i: Int, curOS: OptStruct, ei: Double) = {
-
-    val oS = curOS.add(i, ei)
-    val validEcacheList: Array[Int] = oS.eCache(::, 0).findAll(_ != 0).toArray
+  def selectJ(i: Int, oS: OptStruct, ei: Double): (Int, Double) = {
+    oS.cache(i, ei)
+    val validEcacheList: Array[Int] = oS.validEcacheArr
 
     if (validEcacheList.size > 1) {
+      // loop through valid Ecache values and find the one that maximizes delta E
       val opt = validEcacheList.filter(_ != i).foldLeft(JOpt()) { (jOpt, k) =>
-        val ek = calcEk(oS, k)
+        val ek = oS.calcEk(k)
         val deltaE = abs(ei - ek)
-        if (deltaE > jOpt.maxDeltaE) JOpt(k, deltaE, ek) else jOpt
+        if (deltaE >= jOpt.maxDeltaE) JOpt(k, deltaE, ek) else jOpt
       }
-      (opt.maxK, opt.ej, oS)
+      (opt.maxK, opt.ej)
     } else {
-      val j = SimplifiedSMO.selectJrand(i, oS.rows)
-      println(j)
-      val ej = calcEk(oS, j)
-      (j, ej, oS)
+      val j = selectJrand(i, oS.rows)
+      val ej = oS.calcEk(j)
+      (j, ej)
     }
   }
+
+  def innerL(i: Int, oS: OptStruct) = {
+    val ei = oS.calcEk(i)
+    if ((oS.label(i) * ei < -oS.tolerance && oS.alpha(i) < oS.constant) ||
+      (oS.label(i) * ei > oS.tolerance && oS.alpha(i) > 0)) {
+      val (j, ej) = selectJ(i, oS, ei)
+      val (alphaIold, alphaJold) = (oS.alpha(i), oS.alpha(j))
+      val (low, high) = calcLH(oS, i, j)
+      if (low == high) {
+        println(s"L == H[$low]")
+        (0, oS)
+      } else {
+        // calculate optimal amount to change alpha[j]
+        val eta = oS.calcETA(i, j)
+        if (eta >= 0) {
+          println(s"eta[$eta] >= 0")
+          (0, oS)
+        } else {
+          oS.alphas(j, 0) -= oS.label(j) * (ei - ej) / eta
+          oS.alphas(j, 0) = clipAlpha(oS.alpha(j), high, low)
+          oS.updateEk(j)
+          if (abs(oS.alpha(j) - alphaJold) < 0.00001) {
+            println(s"j not moving enough[${abs(oS.alpha(j) - alphaJold).abs}]")
+            (0, oS)
+          } else {
+            oS.alphas(i, 0) += (oS.label(j) * oS.label(i) * (alphaJold - oS.alpha(j)))
+            oS.updateEk(i)
+
+            val (b1, b2) = calcB(oS, i, alphaIold, ei, j, alphaJold, ej)
+            val newB = {
+              if (oS.alpha(i) > 0 && oS.alpha(i) < oS.constant) b1
+              else if (oS.alpha(j) > 0 && oS.alpha(j) < oS.constant) b2
+              else (b1 + b2) / 2.0
+            }
+            (1, oS.newB(newB))
+          }
+        }
+      }
+    } else (0, oS)
+  }
+
+  def smoP(dataMatIn: Array[Array[Double]], classLabels: Array[Double], c: Double, toler: Double, maxIter: Int): (Mat, Double) = {
+
+    @tailrec
+    def outerL(oS: OptStruct, iter: Int = 0, entireSet: Boolean = true, curAlphaPairsChanged: Int = 0): (Mat, Double) = {
+      if (iter == maxIter && (curAlphaPairsChanged == 0 || entireSet)) {
+        (oS.alphas, oS.b)
+      } else {
+        val (alphaPairsChanged, updatedOS) = if (entireSet) {
+          // go over all values
+          Range(0, oS.rows).foldLeft(0, oS) { case ((totalChanged, curOS), i) =>
+            val (changed, newOS) = innerL(i, curOS)
+            println(s"fullSet, iter: $iter i:$i, pairs changed $totalChanged")
+            (totalChanged + changed, newOS)
+          }
+        } else {
+          // go over non-bound (railed) alphas
+          oS.nonBoundIndices.foldLeft(0, oS) { case ((totalChanged, curOS), i) =>
+            val (changed, newOS) = innerL(i, curOS)
+            println(s"non-bound, iter: $iter i:$i, pairs changed $totalChanged")
+            (totalChanged + changed, newOS)
+           }
+        }
+        // toggle entire set loop
+        val updatedEntireSet = if (entireSet) false else if (alphaPairsChanged == 0) true else entireSet
+        println(s"iteration number: $iter")
+        outerL(updatedOS, iter + 1, updatedEntireSet, alphaPairsChanged)
+      }
+    }
+
+    outerL(OptStruct(
+      dataMat = DenseMatrix(dataMatIn: _*),
+      labelMat = DenseMatrix(classLabels).t,
+      alphas = DenseMatrix.zeros[Double](dataMatIn.size, 1),
+      b = 0.0, constant = c, tolerance = toler))
+  }
+
+  private def calcB(os: OptStruct, i: Int, alphaIold: Double, ei: Double, j: Int, alphaJold: Double, ej: Double): (Double, Double) = {
+
+    val b1 = os.b - ei - (os.label(i) * (os.alpha(i) - alphaIold)) *
+      (os.dataMat(i, ::) * os.dataMat(i, ::).t: Mat)(0, 0) -
+      (os.label(j) * (os.alpha(j) - alphaJold)) * (os.dataMat(i, ::) *
+        os.dataMat(j, ::).t: Mat)(0, 0)
+
+    val b2 = os.b - ej - (os.label(i) * (os.alpha(i) - alphaIold)) *
+      (os.dataMat(i, ::) * os.dataMat(j, ::).t: Mat)(0, 0) -
+      (os.label(j) * (os.alpha(j) - alphaJold)) * (os.dataMat(j, ::) *
+        os.dataMat(j, ::).t: Mat)(0, 0)
+    (b1, b2)
+  }
+
+  private def calcLH(os: OptStruct, i: Int, j: Int): (Double, Double) = {
+    if (os.label(i) != os.label(j)) {
+      val dev = os.alpha(j) - os.alpha(i)
+      (scala.math.max(0.0, dev), scala.math.min(os.constant, os.constant + dev))
+    } else {
+      val total = os.alpha(j) + os.alpha(i)
+      (scala.math.max(0.0, total - os.constant), scala.math.min(os.constant, total))
+    }
+  }
+
+  // following code is as same as SimplifiedSMO
+
+  def selectJrand(i: Int, m: Int): Int = {
+    val rand = Uniform(0, m)
+    @tailrec
+    def step(i: Int, j: Int): Int = if (i != j) j else step(i, rand.sample().toInt)
+    step(i, rand.sample().toInt)
+  }
+
+  def clipAlpha(aj: Double, h: Double, l: Double): Double = if (aj > h) h else if (aj < l) l else aj
 }
