7f93933cf962a908d708fa32ce3b1a1d867c75f6
noboru.kudoh <noboru.kudoh@gmail.com>
2013-11-05 23:20:17 +0900

    replace dot to matrix *

--- src/main/scala/mlia/lr/LogisticRegression.scala
+++ src/main/scala/mlia/lr/LogisticRegression.scala
@@ -6,35 +6,38 @@ import breeze.stats.distributions.Uniform
 
 object LogisticRegression {
 
+  type Mat = DenseMatrix[Double]
+  type Vec = DenseVector[Double]
+
   val sigmoid = breeze.generic.UFunc((x: Double) => 1.0 / (1 + exp(-1 * x)))
 
-  def gradAscent(dataMatIn: List[Array[Double]], classLabels: Array[Int]): DenseVector[Double] = {
+  def gradAscent(dataMatIn: List[Array[Double]], classLabels: Array[Int]): Vec = {
 
     val alpha = 0.001
     val maxCycle = 500
     val dataMatrix = DenseMatrix(dataMatIn: _*)
-    val labelMat = DenseVector(classLabels.map(_.toDouble))
+    val labelMat = DenseMatrix(classLabels.map(_.toDouble)).t
 
     Range(0, maxCycle).foldLeft(DenseMatrix.ones[Double](dataMatrix.cols, 1)) { (curWeight, cycle) =>
-      val h = sigmoid(dot(dataMatrix, curWeight))
-      val error = labelMat :- h
-      curWeight :+ dot(dataMatrix.t :* alpha, error.toDenseMatrix).toDenseMatrix
+      val h = sigmoid(dataMatrix * curWeight)
+      val error: Mat = labelMat :- h
+      curWeight :+ (dataMatrix.t :* alpha: Mat) * error
     }.toDenseVector
   }
 
-  def stocGradAscent0(dataMatIn: List[Array[Double]], classLabels: Array[Int]): DenseVector[Double] = {
+  def stocGradAscent0(dataMatIn: List[Array[Double]], classLabels: Array[Int]): Vec = {
     val alpha = 0.01
     dataMatIn.zipWithIndex.foldLeft(DenseVector.ones[Double](dataMatIn.head.size)) { case (curWeight, (row, i)) =>
       val vec = DenseVector(row)
-      val h = sigmoid(dot(vec.toDenseMatrix, curWeight.toDenseMatrix).sum)
+      val h = sigmoid((vec.toDenseMatrix * curWeight.toDenseMatrix.t: Mat).sum)
       val error = classLabels(i) - h
-      curWeight :+ (vec :* (alpha * error): DenseVector[Double])
+      curWeight :+ (vec :* (alpha * error): Vec)
     }
   }
 
   def stocGradAscent1Iter500(dataMatIn: List[Array[Double]], classLabels: Array[Int]) = stocGradAscent1(dataMatIn, classLabels, 500)
 
-  def stocGradAscent1(dataMatIn: List[Array[Double]], classLabels: Array[Int], numIter: Int = 150): DenseVector[Double] = {
+  def stocGradAscent1(dataMatIn: List[Array[Double]], classLabels: Array[Int], numIter: Int = 150): Vec = {
 
     Range(0, numIter).foldLeft(DenseVector.ones[Double](dataMatIn.head.size)) { (outerState, i) =>
       Range(0, dataMatIn.size).foldLeft((outerState, Range(0, dataMatIn.size).toArray)) { case ((curWeights, indices), j) =>
@@ -42,10 +45,10 @@ object LogisticRegression {
         val randIndex = Uniform(0, indices.size).sample().toInt
         val vec = DenseVector(dataMatIn(randIndex))
 
-        val h = sigmoid((vec :* curWeights: DenseVector[Double]).sum)
+        val h = sigmoid((vec :* curWeights: Vec).sum)
         val error = classLabels(randIndex) - h
 
-        val newWeights = (curWeights :+ (vec :* (alpha * error): DenseVector[Double]), indices.tail)
+        val newWeights = (curWeights :+ (vec :* (alpha * error): Vec), indices.tail)
         newWeights
       }._1
     }
@@ -55,7 +58,4 @@ object LogisticRegression {
     val prob = sigmoid((inX :* weights).sum)
     if (prob > 0.5) 1 else 0
   }
-
-  private def dot(x: DenseMatrix[Double], y: DenseMatrix[Double]): DenseVector[Double] =
-    DenseVector(Range(0, x.rows).map(i => (x(i, ::) :* y.t).sum).toArray)
 }
